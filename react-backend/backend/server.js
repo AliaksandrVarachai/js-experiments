873dd6ae4eea0d8569bf217162195ba1ef81ddc5
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2018-05-25 20:07:17 +0300

    server works

--- react-backend/backend/server.js
+++ react-backend/backend/server.js
@@ -1,4 +1,8 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const staticDir = path.resolve(__dirname, '../static');
 
 const server = http.createServer();
 
@@ -13,7 +17,24 @@ server.on('request', (req, res) => {
 
   if (method === 'GET') {
     res.setHeader('Content-Type', 'text/plain');
-    res.end('ok');
+    if (url === '/' || '/index' || '/index.html') {
+      const pathname = path.resolve(staticDir, 'index.html');
+      fs.exists(pathname, exists => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.end(`File ${pathname} is not found`);
+          return;
+        }
+        fs.readFile(path.resolve(staticDir, 'index.html'), (err, data) => {
+          res.setHeader('Content-Type', 'text/html');
+          res.write(data);
+          res.end();
+        });
+      });
+    } else {
+      res.whiteHead(400, {'Content-Type': 'text/html'});
+      res.end('no data');
+    }
   }
 
   if (method === 'POST') {
