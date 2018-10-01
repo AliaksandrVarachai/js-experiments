e7613d8e5c9560cc3582dae7bfbce3c36c395b15
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2018-10-01 18:51:14 +0300

    Subsribe iframe's callbacks to parent events

--- cross-origin-communication/frame-1-script.js
+++ cross-origin-communication/frame-1-script.js
@@ -1,66 +1,99 @@
-const messageToSendNode = document.getElementById('message-to-send');
-const receivedMessageNode = document.getElementById('received-message');
-const sendNode = document.getElementById('send');
+let eventBusPort2;  // TODO: move to property of eventBus
 
-let parentPort;
+function EventBus() {
+  this.events = {}; // {name: Arrays.<function>}
+}
 
-sendNode.onclick = function(event) {
-  console.log('Container says: ' + messageToSendNode.value);
-  //window.parent.postMessage(messageToSendNode.value, '*');
-  parentPort.postMessage(messageToSendNode.value);
-};
-
-// fires just first time (when port is transferred to the current window)
-window.onmessage = function(event) {
-  parentPort = event.ports[0];
-  parentPort.onmessage = function(event) {
-    receivedMessageNode.innerText = event.data;
-  };
-};
-
-// Message for all other contexts "I am loaded"
-// when window.onmessage listener is ready to accept a ChannelPort object
-// setTimeout(function() {
-//   window.parent.postMessage('READY', '*');
-// }, 3000);
-
-
-// EventBus listener
-let eventBusPort2;
+const eventBus = new EventBus(); // must be a singleton
 
 const portPostMessageListener = function(event) {
   if (event.data !== 'PORT') // check the origin
     return;
   window.removeEventListener('message', portPostMessageListener);
   eventBusPort2 = event.ports[0];
+
   eventBusPort2.onmessage = function(event) {
+    const eventNames = ['click', 'dblclick', 'contextmenu'];
+    if (eventNames.indexOf(event.data) > -1) {
+      eventBus.dispatch(event.data);
+    } else {
+      throw Error(`Event name "${event.data}" must be included in [${eventNames.join()}]`);
+    }
     // TODO: only eventBus events here or check is needed?
     eventBus.dispatch(event.data); // fires local eventBus Event
-    ********
-  }
+  };
+
+  EventBus.prototype.addEventListener = function(name, cb) {
+    if (this.events[name]) {
+      this.events[name].push(cb);
+    } else {
+      this.events[name] = [cb];
+      eventBusPort2.postMessage(`addEventListener:${name}`); // 1st time only
+    }
+    //console.log(`Callback is added for "${name}" event`);
+  };
+
+  EventBus.prototype.removeEventListener = function(name, cb) {
+    if (!this.events[name])
+      throw Error(`There is no "${name}" event listener`);
+
+    const removeEvent = name => {
+      //const { [name]: _, ...res } = this.events;
+      this.events = Object.keys(this.events).reduce((acc, key) => {
+        if (key !== name)
+          acc[key] = this.events[key];
+        return acc;
+      }, {});
+    };
+
+    const callbacks = this.events[name];
+    if (cb) {
+      const inx = callbacks.indexOf(cb);
+      if (inx < 0)
+        throw Error(`There is no passed callback in "${name}" event listener`);
+      if (inx === 0 && callbacks.length === 1) {
+        removeEvent(name);
+        eventBusPort2.postMessage(`removeEventListener:${name}`); // no listeners anymore
+      } else {
+        callbacks.splice(inx, 1);
+      }
+    } else {
+      removeEvent(name);
+      eventBusPort2.postMessage(`removeEventListener:${name}`); // no listeners anymore
+    }
+    // console.log(`Callback is removed from "${name}" event`);
+  };
+
+  EventBus.prototype.dispatch = function(name) {
+    if (!this.events[name])
+      throw Error(`Dispatched "${name}" events is not found`);
+    this.events[name].forEach(cb => cb());
+    // console.log(`Dispatch for "${name}" is called`)
+  };
+
+  // Tests
+  const click1 = function(e) { console.log('frame:click from parent #1'); };
+  const click2 = function(e) { console.log('frame:click from parent #2'); };
+  const click3 = function(e) { console.log('frame:click from parent #3'); };
+  const dblclick = function(e) { console.log('frame:dblclick from parent'); };
+  const contextmenu = function(e) { console.log('frame:contextmenu from parent'); };
+  eventBus.addEventListener('click', click1);
+  eventBus.addEventListener('click', click2);
+  eventBus.addEventListener('click', click3);
+  eventBus.addEventListener('dblclick', dblclick);
+  eventBus.addEventListener('contextmenu', contextmenu);
+
+  setTimeout(() => {
+    eventBus.removeEventListener('click', click1);
+    eventBus.removeEventListener('click', click2);
+    eventBus.removeEventListener('click', click3);
+    eventBus.removeEventListener('dblclick', dblclick);
+    eventBus.removeEventListener('contextmenu', contextmenu);
+  }, 5000)
 };
 
-window.addEventListener('message', portPostMessageListener);
+window.addEventListener('message', portPostMessageListener); // waiting for the 'PORT' message
 
-window.parent.postMessage('READY', '*');
-
-
-const generateId = (function generateId() {
-  let counter = 0;
-  return () => counter++;
-})();
-
-function EventBus() {
-  this.listeners = {};
-}
-
-eventBus.prototype.addEventListener = function(name, callback, scope) {
-  if (scope) {
-    eventBusPort2.postMessage(name, generateId());
-    *****
-  } else {
-    // like without iframes
-  }
-};
+window.parent.postMessage('READY', '*'); // 'I am ready'
 
 
--- cross-origin-communication/script.js
+++ cross-origin-communication/script.js
@@ -1,122 +1,31 @@
-const messageToSendNode = document.getElementById('message-to-send');
-const receivedMessageNode = document.getElementById('received-message');
-const sendNode = document.getElementById('send');
-
-const channel = new MessageChannel();
-let frameIsLoaded = false;
-
-sendNode.onclick = function(event) {
-  console.log('Container says: ' + messageToSendNode.value);
-  if (!frameIsLoaded) {
-    console.log('Wait a bit while frames are loading');
-    return;
-  }
-  channel.port1.postMessage(messageToSendNode.value)
-};
-
-// Listen iframe
-window.onmessage = function(event) {
-  if (event.origin !== 'http://localhost:9080') {
-    console.warn(`${event.origin} is not allowed`);
-    return;
-  }
-  frameIsLoaded = true;
-  window.frames[0].postMessage(messageToSendNode.value, event.origin, [channel.port2]);
-  channel.port1.onmessage = function(event) {
-    // It is not necessary to check an event.origin
-    receivedMessageNode.innerText = event.data;
-  };
-};
-
-// EventBus Channel
 const eventBusChannel = new MessageChannel();
 const eventBusPort1 = eventBusChannel.port1;
 
+const eventNames = {};
+
 eventBusPort1.onmessage = function(event) {
-  console.log('Port1 received: ' + event.data);
+  // console.log('Port1 received: ' + event.data);
+  const [eventListenerMethod, eventName] = event.data.split(':');
+  if (!eventListenerMethod || !eventName)
+    throw Error(`Post message cannot be converted to eventListenerMethod and eventName: "${event.data}"`);
+  // eventListenerMethod = 'addEventListener' || 'removeEventListener'
+  if (eventListenerMethod === 'addEventListener') {
+    // eventBus is singleton, so only every listener will be added only once
+    eventNames[eventName] = function(event) {
+      eventBusPort1.postMessage(eventName);
+    }
+  }
+  document[eventListenerMethod](eventName, eventNames[eventName]);
+  if (eventListenerMethod === 'removeEventListener') {
+    // eventBus is singleton, so only every listener will be added only once
+    eventNames[eventName] = null;
+  }
+
 };
 
-const readyPostMessageListener = function(event) {
+window.addEventListener('message', function readyPostMessageListener(event) {
   if (event.data !== 'READY') // check the origin
     return;
   frames[0].postMessage('PORT', event.origin, [eventBusChannel.port2]);
   window.removeEventListener('message', readyPostMessageListener);
-};
-
-window.addEventListener('message', readyPostMessageListener);
-
-
-function EventBus() {
-  this.events = {};
-}
-
-EventBus.prototype.addEventListener = function(name, cb) {
-  if (this.events[name]) {
-    this.events[name].push(cb)
-  } else {
-    this.events[name] = [cb];
-  }
-  console.log(`Callback is added for "${name}" event`);
-};
-
-EventBus.prototype.removeEventListener = function(name, cb) {
-  if (!this.events[name])
-    throw Error(`There is no "${name}" event listener`);
-
-  const removeEvent = name => {
-    //const { [name]: _, ...res } = this.events;
-    this.events = Object.keys(this.events).reduce((acc, key) => {
-      if (key !== name)
-        acc[key] = this.events[key];
-      return acc;
-    }, {})
-  };
-
-  const callbacks = this.events[name];
-  if (cb) {
-    const inx = callbacks.indexOf(cb);
-    if (inx < 0)
-      throw Error(`There is no passed callback in "${name}" event listener`);
-    if (inx === 0 && callbacks.length === 1) {
-      removeEvent(name);
-    } else {
-      callbacks.splice(inx, 1);
-    }
-  } else {
-    removeEvent(name);
-  }
-  console.log(`Callback is removed from "${name}" event`);
-};
-
-EventBus.prototype.dispatch = function(name) {
-  if (!this.events[name])
-    throw Error(`Dispatched "${name}" events is not found`);
-  this.events[name].forEach(cb => cb());
-  console.log(`Dispatch for "${name}" is called`)
-};
-
-// tests
-const eventBus = new EventBus(); // must be a singletone
-const timerEventName = 'timer';
-let counter = 0;
-
-const start = Date.now();
-const timerInterval = setInterval(() => {
-
-  const callback = () => {
-    console.log(`listener ${counter++} is called`);
-  };
-
-  setTimeout(() => {
-    eventBus.removeEventListener(timerEventName, callback);
-  }, 1500);
-
-  eventBus.addEventListener(timerEventName, callback);
-
-  if (Date.now() > start + 5000)
-    clearInterval(timerInterval);
-}, 1000);
-
-setTimeout(() => {
-  eventBus.dispatch(timerEventName);
-}, 3000);
+});
