d6324bf014b96e4914c94a02cfcf0da668ee0418
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2019-07-15 17:59:07 +0300

    working square-sum with bit operations

--- codewars/square-sum.js
+++ codewars/square-sum.js
@@ -36,63 +36,71 @@ function square_sums_row(n){
 
   //traverse all nodes
   var path = new Array(n).fill(0);
+  var bitPath = new Int32Array(Math.ceil(n / 32)).fill(0); // 32 x 32 = 1024 bit maximum (to fast check path elements)
   var pathIndex = -1;
   if (boundNodes.length > 0) {
     // start with boundNodes[0] node
+    console.log(`fast ${n}:${i}`)
     pathIndex++;
     path[0] = boundNodes[0];
+    setPathBit(boundNodes[0]);
     traverseDFS(edges[boundNodes[0] - 1]);
     return pathIndex === n - 1 ? path : false;
   }
-  // start with all nodes
+  // brute force checke for all nodes
   for (i = 0; i < maxSquare - 1 >> 1; i++) {
+    console.log(`brute force ${n}:${i}`);
     pathIndex++;
     path[0] = i + 1;
+    setPathBit(i + 1);
     traverseDFS(edges[i]);
     if (pathIndex === n - 1)
       return path;
+    clearPathBit(i + 1);
     pathIndex--;
   }
 
   function traverseDFS(links) {
     for (var i = 0; i < maxSquare - 1; i++) {
-      //debugger;
       var toNode = links[i];
-      if (0 < toNode && toNode <= n && path.indexOf(toNode) === -1) {
+      if (0 < toNode && toNode <= n && !checkPathBit(toNode)) {
         pathIndex++;
         path[pathIndex] = toNode;
+        setPathBit(toNode);
         if (pathIndex === n - 1)
           return;
         traverseDFS(edges[toNode - 1]);
         if (pathIndex === n - 1)
           return;
         path[pathIndex] = 0;
+        clearPathBit(toNode);
         pathIndex--;
       }
     }
   }
 
-  var maxNumber = 1000;
-  var bitArray = Int32Array(Math.ceil(maxNumber / 32)).fill(0); // 1024 bit max
-  function setBit(b) {
-    bitArray[b >> 5] |= 1 << (~(b >> 5) << 5); // 1 << b % 32;
+  function setPathBit(b) {
+    bitPath[b >> 5] |= 1 << (b << 27 >>> 27); // 1 << b % 32;
   }
 
-  function deleteBit(b) {
-    bitArray[b >> 5] ^= 1 << (~(b >> 5) << 5); // 1 << b % 32;
+  function checkPathBit(x) {
+    return bitPath[x >> 5] & 1 << (x << 27 >>> 27);
   }
 
-  function areFirstSet(x) {
-    var lastFullBitNumber = x >> 5 - 1;
+  function clearPathBit(b) {
+    bitPath[b >> 5] &= ~(1 << (b << 27 >>> 27)); // 1 << b % 32;
+  }
+
+  function checkFullPath(x) {
+    var lastFullBitNumber = x >> 5;
     var i;
     for (i = 0; i < lastFullBitNumber; i++) {
-      if (bitArray[i] !== ~0)
+      if (bitPath[i] ^ ~0)
         return false;
     }
-    return (~0 >>> (x >> 5) | bitArray[i]) === ~0;  // 1 << b % 32;
+    var mask = ~(~0 << (x << 27 >>> 27));
+    return !(bitPath[i] ^ mask);
   }
-
-
 
   return pathIndex === n - 1 ? path : false;
 }
