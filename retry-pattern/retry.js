f60cce842f83d491d6758e267d38b94c71efcd03
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2019-09-19 12:44:13 +0300

    Implement promise and callback versions

--- retry-pattern/retry.js
+++ retry-pattern/retry.js
@@ -1,35 +1,25 @@
 const defaultOptions = {
-  args: [],        // e.g. [url, callback] or [url] for callback and promise versions responsively
   interval: 1000,  // ms, or (attemptCounter) => 100 * attemptCounter
   maxAttempts: 0,  // 0 - infinite
   maxDuration: 0,  // 0 - infinite,
   isPromise: true, // false for callback version
 };
 
+
 function retry(func, customOptions = {}) {
   const options = { ...defaultOptions, ...customOptions };
   let attemptCounter = 0;
   const startTime = Date.now();
 
-  if (options.isPromise) {
-    return retriedPromise();
-  } else {
-    retriedCallback();
-  }
-
-
-  function retriedPromise() {
-    console.log(`attempt: ${attemptCounter}`);
+  function retryPromise(...args) {
     ++attemptCounter;
     return new Promise((resolve, reject) => {
-      func(...options.args)
-        .then(response => {
-          resolve(response);
-        })
+      func(...args)
+        .then(response => resolve(response))
         .catch(err => {
           const rejectTime = Date.now();
           if (isNewAttemptPossible(rejectTime)) {
-            setTimeout(() => resolve(retriedPromise()), getWaitingTimeBeforeNewAttempt(rejectTime));
+            setTimeout(() => resolve(retryPromise(...args)), getWaitingTimeBeforeNewAttempt(rejectTime));
           } else {
             reject(err);
           }
@@ -38,29 +28,25 @@ function retry(func, customOptions = {}) {
   }
 
 
-  function retriedCallback() {
-    // const callback = options.args[options.args.length - 1];
-    // if (!callback || typeof callback !== 'function')
-    //   throw Error('Callback is not provided as last element of options.args[] or does not have function type.');
-    // const argsWithoutCallback = options.args.filter(arg => arg !== callback);
+  function retryCallback(...args) {
+    const callback = args[args.length - 1];
+    if (!callback || typeof callback !== 'function')
+      throw Error('Callback must be provided as the last argument.');
+    const argsWithoutCallback = args.slice(0, args.length - 1);
 
-    function proxyCallback(err, ...dataArgs) {
-      ++attemptCounter;
+    function proxyCallback(err, ...data) {
       if (err) {
-        const errorTime = Date.now();
-        if (isNewAttemptPossible(errorTime)) {
-          setTimeout(() => {
-            func(...argsWithoutCallback, proxyCallback);
-          }, getWaitingTimeBeforeNewAttempt(errorTime));
+        const rejectTime = Date.now();
+        if (isNewAttemptPossible(rejectTime)) {
+          setTimeout(() => func(...argsWithoutCallback, proxyCallback), getWaitingTimeBeforeNewAttempt(rejectTime));
         } else {
           callback(err);
         }
       } else {
-        callback(null, ...dataArgs);
+        callback(null, ...data);
       }
     }
 
-    debugger;
     func(...argsWithoutCallback, proxyCallback);
   }
 
@@ -77,13 +63,16 @@ function retry(func, customOptions = {}) {
       : options.interval * attemptCounter;
     return requiredIntervalFromStart - (errorTime - startTime);
   }
+
+  
+  return options.isPromise ? retryPromise : retryCallback;
 }
 
 
 
 // test promise version (default settings)
 let counter1 = 0;
-function testPromiseVersion() {
+function promisifiedFunc() {
   ++counter1;
   return new Promise((resolve, reject) => {
     if (counter1 > 5)
@@ -93,7 +82,11 @@ function testPromiseVersion() {
   });
 }
 
-retry(testPromiseVersion)
+const options1 = {
+  maxAttempts: 2
+};
+const retriedPromise = retry(promisifiedFunc);
+retriedPromise(options1)
   .then(data => console.log(`Success! Received data: ${data}`))
   .catch(err => console.log(err.message));
 
@@ -101,7 +94,7 @@ retry(testPromiseVersion)
 // test callback version
 
 let counter2 = 0;
-function testCallbackVersion(callback) {
+function callbackifiedFunc(callback) {
   ++counter2;
   if (counter2 > 10)
     callback(null, '2^256');
@@ -109,13 +102,14 @@ function testCallbackVersion(callback) {
     callback(new Error('Callback: Get off me!'))
 }
 
-retry(testCallbackVersion((err, data) => {
+const options2 = {
+  isPromise: false,
+  maxAttempts: 1,
+};
+const retriedCallback = retry(callbackifiedFunc, options2);
+retriedCallback((err, data) => {
   if (err)
     console.log(err.message);
   else
     console.log(data);
-}), {
-  isPromise: false,
-  maxAttempts: 5,
 });
-
