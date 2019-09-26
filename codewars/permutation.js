81bbb20bef9552debe570173bd0fc1159cf2b20c
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2019-09-26 18:31:48 +0300

    Add permutation cases

--- codewars/permutation.js
+++ codewars/permutation.js
@@ -21,12 +21,19 @@ function permutation(n) {
     return true;
   }
 
-  console.log(a.join(','));
+  console.log(a.join(''));
   while (next())
-    console.log(a.join(','));
+    console.log(a.join(''));
 }
 
 
+// "11122"
+// "11212"
+// "11221"
+// "12112"
+// "12121"
+// "12211"
+// "21112"
 function permutationsWithEquals(a, compare = undefined) {
   var n = a.length;
   var s = a.slice()
@@ -34,7 +41,7 @@ function permutationsWithEquals(a, compare = undefined) {
 
   function next() {
     var i = n - 2;
-    while (i >= 0 && s[i] >= s[i + 1])
+    while (i >= 0 && s[i] >= s[i + 1]) // just '=' sign for equal elements!!!
       --i;
     if (i < 0)
       return false;
@@ -47,12 +54,92 @@ function permutationsWithEquals(a, compare = undefined) {
     return true;
   }
 
-  console.log(s);
+  console.log(s.join(''));
   while (next()) {
-    console.log(s);
+    console.log(s.join(''));
   }
+}
+
+// n elements on n positions (elements of a will be used for output):
+// "000"
+// "001"
+// "002"
+// "010"
+// "011"
+// "012"...
+function permutationsRepetition(a) {
+  var n = a.length;
+  var s = [];
+  for (var i = 0; i < n; ++i)
+    s.push(0);
+  var inx = n - 1;
+
+  function next() {
+    if (s[inx] < n - 1) {
+      ++s[inx];
+    } else {
+      for (var i = n - 1; i >= inx; --i)
+        s[i] = 0;
+      --inx;
+      while (inx >= 0 && s[inx] === n - 1) {
+        s[inx] = 0;
+        --inx;
+      }
+      if (inx < 0)
+        return false;
+      ++s[inx];
+      inx = n - 1;
+    }
+    return true;
+  }
+
+  console.log(s.map(i => a[i]).join(''));
+  while(next())
+    console.log(s.map(i => a[i]).join(''));
+}
+
+// "000"
+// "001"
+// "002"
+// "010"
+// "011"
+// "012"
+// "020"
+// "021"
+// "022"
+function sampleWithRepeatition(a, k) { // k elements form a[] (k < a.length)
+  var n = a.length;
+  var s = [];
+  for (var i = 0; i < k; ++i)
+    s.push(0);
+  var inx = k - 1;
+
+  function next() {
+    if (s[inx] < n - 1) {
+      ++s[inx];
+    } else {
+      for (var i = k - 1; i >= inx; --i)
+        s[i] = 0;
+      --inx;
+      while (inx >= 0 && s[inx] === n - 1) {
+        s[inx] = 0;
+        --inx;
+      }
+      if (inx < 0)
+        return false;
+      ++s[inx];
+      inx = k - 1;
+    }
+    return true;
+  }
+
+  console.log(s.map(i => a[i]).join(''));
+  while(next())
+    console.log(s.map(i => a[i]).join(''));
 }
 
 
 permutation(5);
 permutationsWithEquals([1, 1, 2, 1, 2], (a, b) => a - b);
+permutationsRepetition([7, 8, 9]);
+sampleWithRepeatition([0, 1, 2], 3)
