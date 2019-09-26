7521d70e0c374cbee3d7a74ac9287bbabd3a6a13
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2019-09-26 13:30:08 +0300

    Config docker-swarm

---
+++ docker-swarm/test.js
@@ -0,0 +1,8 @@
+
+
+console.log('env.PORT: ', process.env.PORT);
+console.log('argv0: ', process.argv0);
+console.log('execArgv: ', process.execArgv);
+console.log('execPath: ', process.execPath);
+vars = execPath
+console.log('argv[: ', process.argv);
