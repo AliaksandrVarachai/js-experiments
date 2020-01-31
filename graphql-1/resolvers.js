0c8de285f96d3f6ca0e19314e50c9437754432a8
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2020-01-31 21:02:57 +0300

    Add query

--- graphql-1/resolvers.js
+++ graphql-1/resolvers.js
@@ -1,5 +1,22 @@
+const db = require('./db');
+
 const Query = {
-  test: () => 'Test Success, GraphQL server is up & running!!'
+  test: (root, arts, context, info) => 'Test Success, GraphQL server is up & running!!',
+  students: async () => Promise.resolve(db.students.list()),
+  colleges: async () => Promise.resolve(db.colleges.list()),
+  studentById: async (root, args, context, info) => {
+    return db.students.get(args.id)
+  },
+  collegeById: async (root, args, context, info) => db.colleges.get(args.id),
+  // variables
+  sayHello: async (root, args) => `GraphQL says hi to ${args.name}!`,
+  // Enum
+  setFavouriteColor: (root, args) => `Favourite color is ${args.color}.`,
 };
 
-module.exports = { Query };
+const Student = {
+  fullName: async (root, args, context, info) => `${root.firstName}:${root.lastName}`,
+  college: async (root) => db.colleges.get(root.collegeId),
+};
+
+module.exports = { Query, Student };
