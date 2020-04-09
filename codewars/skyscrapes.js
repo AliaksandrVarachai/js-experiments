303fa63c4aab9a5ec349382cbc6ba284233c9214
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2020-04-09 22:04:32 +0300

    Test permutations

--- codewars/skyscrapes.js
+++ codewars/skyscrapes.js
@@ -1,24 +1,22 @@
 function generatePermutations(size = 6) {
   const permutations = [];
   const row = [];
-  let i, j, k;
 
-  for (i = 1; i <= size; ++i) row.push(i);
+  for (let i = 1; i <= size; ++i) row.push(i);
 
   permutations.push(row.slice());
 
   while (true) {
-    i = size - 2;
+    let i = size - 2;
     while (i > -1 && row[i] > row[i + 1]) --i;
     if (i === -1) return permutations;
     //i - эл-т нарушающий убывающую последовательность
-    j = i + 1;
+    let j = i + 1;
     while (j < size && row[j] > row[i]) ++j;
     // j - 1 - эл-т с которым меняем нарушающий эл-т
     [row[i], row[j - 1]] = [row[j - 1], row[i]];
-    for (k = i + 1; k < size - 1 - k; ++k) {
-      [row[k], row[size - 1 - k]] = [row[size - 1 - k], row[k]];
-    }
+    for (let k = i + 1, d = size - 1; k < d; ++k, --d)
+      [row[k], row[d]] = [row[d], row[k]];
     // начиная с i + 1 все элементы теперь упорядочены по возрастанию
     permutations.push(row.slice());
   }
@@ -26,7 +24,7 @@ function generatePermutations(size = 6) {
   return permutations;
 }
 
-const permutations = generatePermutations(3);
+const permutations = generatePermutations(7);
 permutations.forEach(perm => console.log(perm.join()));
 
 
