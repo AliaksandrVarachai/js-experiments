e366a3200abb7000e871e440d08dac8de52b904f
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2023-10-02 13:54:13 +0300

    Add memoized solution

--- codewars/eggs.js
+++ codewars/eggs.js
@@ -1,10 +1,20 @@
 // JS bigInts has already been preloaded in constant BigNumber
 // Usage: https://github.com/MikeMcl/bignumber.js/
+
 module.exports.default = function height(n,m) {
+  const memoizedHeight = [];
+  for (let i = 0; i < m; ++i) memoizedHeight[i] = [0, 1];
+  for (let j = 1; j < m; ++j) {
+    memoizedHeight[0][j] = 0;
+    memoizedHeight[1][j] = j;
+  }
+
+
   function _height(n,m) {
-    if (n === 1) return m;
-    if (m === 1) return 1;
-    return _height(n, m-1) + _height(n-1, m-1) + 1;
+    if (memoizedHeight[n][m]) return memoizedHeight[n][m];
+    const h = _height(n, m-1) + _height(n-1, m-1) + 1;
+    memoizedHeight[n][m] = h;
+    return h;
   }
 
   if (n === 0 || m === 0) return 0;
--- codewars/eggs.test.js
+++ codewars/eggs.test.js
@@ -18,7 +18,7 @@ test('Should run test #4', () => {
   assert.strictEqual(eggs(7,20), 137979);
 });
 
-test.skip('Should run test #5', () => {
+test('Should run test #5', () => {
   assert.strictEqual(eggs(7,500), 1_507_386_560_013_475);
   //                                   9_007_199_254_740_991
 });
