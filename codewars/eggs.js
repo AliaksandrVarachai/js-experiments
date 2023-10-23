591b22ad6a570949bc03b5baeb4ef833cbbc9b5c
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2023-10-23 23:28:35 +0300

    Sync

--- codewars/eggs.js
+++ codewars/eggs.js
@@ -1,24 +1,33 @@
 // JS bigInts has already been preloaded in constant BigNumber
 // Usage: https://github.com/MikeMcl/bignumber.js/
+let prevValue;
+
+function coef(m, n) {
+  if (m === 0) return 1;
+  if (m === 1) {
+    prevValue = n;
+    return prevValue;
+  }
+  console.log(`******** prevValue=`, prevValue, m, n);
+  prevValue = prevValue * (n - m) / (m + 1);
+  return prevValue;
+
+  // let result = 1;
+  // for (let i = 0; i < m; ++i) {
+  //     result *= (n - i) / (i + 1);
+  // }
+  // return result;
+}
 
 module.exports.default = function height(n,m) {
-  const memoizedHeight = [];
-  for (let i = 0; i < m; ++i) memoizedHeight[i] = [0, 1];
-  for (let j = 1; j < m; ++j) {
-    memoizedHeight[0][j] = 0;
-    memoizedHeight[1][j] = j;
-  }
-
-
-  function _height(n,m) {
-    if (memoizedHeight[n][m]) return memoizedHeight[n][m];
-    const h = _height(n, m-1) + _height(n-1, m-1) + 1;
-    memoizedHeight[n][m] = h;
-    return h;
-  }
-
   if (n === 0 || m === 0) return 0;
-  return _height(n,m);
+  let sum = 0;
+  for (let i = 1; i <= n; ++i) {
+    sum += coef(i, m);
+  }
+  return sum;
   // if (n === 0 || m === 0) return new BigNumber(0);
   // return new BigNumber(_height(n,m));
 }
+
+module.exports.coef = coef;
--- codewars/eggs.test.js
+++ codewars/eggs.test.js
@@ -1,26 +1,40 @@
 const test = require('node:test');
 const assert = require('node:assert');
-const eggs = require('./eggs').default;
+const { default: eggs, coef } = require('./eggs');
 
-test('Should run test #1', () => {
+test('Should chack coefs', () => {
+  const results = [];
+  results[0] = coef(0,5);
+  results[1] = coef(1,5);
+  results[2] = coef(2,5);
+  let sum = 0;
+  for (const res of results) sum += res;
+
+  assert.strictEqual(results[0], 1);
+  assert.strictEqual(results[1], 5);
+  assert.strictEqual(results[2], 10);
+  assert.strictEqual(results[3], 10);
+});
+
+test.skip('Should run test #1', () => {
   assert.strictEqual(eggs(0,14), 0);
 });
 
-test('Should run test #2', () => {
+test.skip('Should run test #2', () => {
   assert.strictEqual(eggs(2,0), 0);
 });
 
-test('Should run test #3', () => {
+test.skip('Should run test #3', () => {
   assert.strictEqual(eggs(2,14), 105);
 });
 
-test('Should run test #4', () => {
+test.skip('Should run test #4', () => {
   assert.strictEqual(eggs(7,20), 137979);
 });
 
-test('Should run test #5', () => {
+test.skip('Should run test #5', () => {
   assert.strictEqual(eggs(7,500), 1_507_386_560_013_475);
-  //                                   9_007_199_254_740_991
+  //                                 max 9_007_199_254_740_991
 });
 
 
